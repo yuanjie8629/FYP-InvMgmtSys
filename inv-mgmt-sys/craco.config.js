8ccae71916aa96effa4d133db880a02b9e6763c2
Tan Yuan Jie <yuanjie8629@gmail.com>
2021-10-16 03:12:20 +0800

    First commit

---
+++ inv-mgmt-sys/craco.config.js
@@ -0,0 +1,32 @@
+const CracoLessPlugin = require("craco-less");
+const path = require("path");
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              hack: `true; @import "${path.resolve(__dirname, "theme.less")}";`,
+            },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+  babel: {
+    plugins: [
+      [
+        "import",
+        {
+          libraryName: "antd",
+          libraryDirectory: "es",
+          style: true,
+        },
+        "antd",
+      ],
+    ],
+  },
+};
