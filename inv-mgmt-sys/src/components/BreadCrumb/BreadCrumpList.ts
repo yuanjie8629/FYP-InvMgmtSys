f849149d97fb7d3856971c77909a14ffe86ee1cb
Tan Yuan Jie <yuanjie8629@gmail.com>
2021-11-02 00:47:57 +0800

    Modify

---
+++ inv-mgmt-sys/src/components/BreadCrumb/BreadCrumpList.ts
@@ -0,0 +1,8 @@
+const breadcrumbNameMap = {
+  '/dashboard': 'Dashboard',
+  '/product/management': 'Product Management',
+  '/product/inventory': 'Product Inventory',
+  '/product/add': 'Add Product',
+  '/package/management': 'Package Management',
+  '/package/inventory': 'Package Inventory',
+}
