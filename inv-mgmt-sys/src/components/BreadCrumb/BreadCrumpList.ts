3520fe7940a153c3ba4b780e74acfdfc794d5cf1
Tan Yuan Jie <yuanjie8629@gmail.com>
2021-11-03 00:28:04 +0800

    modify breadcrump

--- inv-mgmt-sys/src/components/BreadCrumb/BreadCrumpList.ts
+++ inv-mgmt-sys/src/components/BreadCrumb/BreadCrumpList.ts
@@ -5,4 +5,23 @@ const breadcrumbNameMap = {
   '/product/add': 'Add Product',
   '/package/management': 'Package Management',
   '/package/inventory': 'Package Inventory',
+  '/order/management': 'Order Management',
+  '/order/pending': 'Pending Orders',
+  '/order/add': 'Add Order',
+  '/shipment/management': 'Shipment Management',
+  '/shipment/shipping-fee': 'Shipping Fee Management',
+  '/customer/management': 'Customer Management',
+  '/customer/registrations': 'Agent/Dropshipper Registration Management',
+  '/customer/add': 'Add Customer',
+  '/business-insights/statistics': 'Statistics',
+  '/business-insights/inventory-analysis': 'Inventory Analysis',
+  '/discount/management': 'Discount Management',
+  '/discount/add': 'Add Discount',
+  '/my-acc/profile-management': 'Profile Management',
+  '/my-acc/acc-settings': 'Account Settings',
+  '/admin/management': 'Admin Management',
+  '/admin/add': 'Add Admin',
+  '/help': 'Help',
 }
+
+export default breadcrumbNameMap
--- inv-mgmt-sys/src/pages/BusinessInsights/InvAnalysis.tsx
+++ inv-mgmt-sys/src/pages/BusinessInsights/InvAnalysis.tsx
@@ -1,5 +1,4 @@
 import Layout from '@components/Layout/Layout';
-
 const InvAnalysis = () => {
   return <Layout>Inventory Analysis</Layout>;
 };
