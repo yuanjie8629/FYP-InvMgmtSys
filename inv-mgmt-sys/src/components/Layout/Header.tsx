403be35e140b11401ae410147cad3acdf9c2f3c7
Tan Yuan Jie <yuanjie8629@gmail.com>
2021-11-03 00:28:04 +0800

    modify breadcrump

--- inv-mgmt-sys/src/components/Layout/Header.tsx
+++ inv-mgmt-sys/src/components/Layout/Header.tsx
@@ -1,9 +1,13 @@
-import { Layout } from 'antd';
-import './Layout.less';
+import { Layout } from 'antd'
+import './Layout.less'
 
 const CustomHeader = () => {
-  const { Header } = Layout;
-  return <Header className='header'></Header>;
-};
+  const { Header } = Layout
+  return (
+    <div className="header-fixed">
+      <Header className="header"></Header>
+    </div>
+  )
+}
 
-export default CustomHeader;
+export default CustomHeader
