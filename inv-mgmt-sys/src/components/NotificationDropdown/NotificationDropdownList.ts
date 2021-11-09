16023a7e753d392ae65a16a766d90eb631a953da
Tan Yuan Jie <yuanjie8629@gmail.com>
2021-11-10 00:51:57 +0800

    Complete header

--- inv-mgmt-sys/src/components/NotificationDropdown/NotificationDropdownList.ts
+++ inv-mgmt-sys/src/components/NotificationDropdown/NotificationDropdownList.ts
@@ -1,17 +1,28 @@
-const menuList = [
+const menuList: {
+  cat: string;
+  status: 'success' | 'error' | 'processing' | 'default' | 'warning';
+  title: string;
+  description: string;
+  timestamp: string;
+  read: boolean;
+}[] = [
   {
     cat: 'order',
     status: 'success',
     title: 'New Order',
-    description: 'q14r12reqr',
+    description:
+      'Tan Yuan Jie has placed new order!\nPlease proceed his order.',
     timestamp: '08-11-2021 16:00',
+    read: false,
   },
   {
     cat: 'product',
     status: 'error',
     title: 'Low Stock',
-    description: 'q14r12reqr',
+    description:
+      'Kari Ayam is currently in low stock.\nPlease consider restocking it.',
     timestamp: '07-11-2021 18:00',
+    read: true,
   },
 ];
 
