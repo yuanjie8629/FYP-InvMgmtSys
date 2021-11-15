0eb461f56a462d28fbba6212b7cd22907d91e79a
YuanJie <yuanjie8629@gmail.com>
2021-11-16 03:36:24 +0800

    Complete Dashboard Chart

--- inv-mgmt-sys/src/components/Chart/LineChart.tsx
+++ inv-mgmt-sys/src/components/Chart/LineChart.tsx
@@ -1,28 +1,38 @@
 import React from 'react';
-import { Line } from '@ant-design/charts';
+import { Datum, Line } from '@ant-design/charts';
 
 const LineChart = () => {
   const data = [
-    { year: '1991', value: 3 },
-    { year: '1992', value: 4 },
-    { year: '1993', value: 3.5 },
-    { year: '1994', value: 5 },
-    { year: '1995', value: 4.9 },
-    { year: '1996', value: 6 },
-    { year: '1997', value: 7 },
-    { year: '1998', value: 9 },
-    { year: '1999', value: 13 },
+    { Month: 'Jan', Sales: 4000 },
+    { Month: 'Feb', Sales: 4500 },
+    { Month: 'Mar', Sales: 5500 },
+    { Month: 'Apr', Sales: 4300 },
+    { Month: 'May', Sales: 4100 },
+    { Month: 'Jun', Sales: 6000 },
+    { Month: 'July', Sales: 4700 },
+    { Month: 'Aug', Sales: 4200 },
+    { Month: 'Sep', Sales: 6100 },
+    { Month: 'Oct', Sales: 5030 },
+    { Month: 'Nov', Sales: 4954 },
+    { Month: 'Dec', Sales: 5310 },
   ];
   const config = {
     data,
     height: 400,
-    xField: 'year',
-    yField: 'value',
-    point: {
-      size: 5,
-      shape: 'diamond',
+    xField: 'Month',
+    yField: 'Sales',
+    smooth: true,
+    lineStyle: {
+      stroke: '#0e9f6e',
+      lineWidth: 4,
     },
-    stroke: '#0e9f6e',
+    color: '#0e9f6e',
+    tooltip: {
+      fields: ['Month', 'Sales'],
+      formatter: (datum: Datum) => {
+        return { name: 'Total Sales', value: 'RM' + datum.Sales.toFixed(2) };
+      },
+    },
   };
   return <Line {...config} />;
 };
