217aff503d2676faabc0802fb3fd7acc83380146
YuanJie <yuanjie8629@gmail.com>
2021-11-18 23:26:50 +0800

    Dashboard Minor Changes

--- inv-mgmt-sys/src/components/ContainerCard/ContainerCard.tsx
+++ inv-mgmt-sys/src/components/ContainerCard/ContainerCard.tsx
@@ -2,14 +2,20 @@ import { Card } from 'antd';
 
 interface ContainerCardProps {
   width?: string | number;
+  height?: string | number;
   children?: React.ReactNode;
 }
-const ContainerCard = ({ width = '95%', ...props }: ContainerCardProps) => {
+const ContainerCard = ({
+  width = '95%',
+  height = '100%',
+  ...props
+}: ContainerCardProps) => {
   return (
     <Card
       bordered={false}
       style={{
         width: width,
+        height: height,
         boxShadow:
           '0px 1px 3px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.06)',
         padding: '10px 25px',
