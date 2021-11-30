031d994b14bdbf7e0c370849738c6909c10c208c
YuanJie <yuanjie8629@gmail.com>
2021-12-01 02:50:32 +0800

    Informative Table 80%

---
+++ inv-mgmt-sys/src/pages/Products/prodList.ts
@@ -0,0 +1,16 @@
+import NasiBriyaniBukhari from '@assets/Product/NasiBukhari.png';
+
+const prodList = [
+    {
+        key: '1',
+        prodNm: 'Nasi Briyani Bukhari',
+        prodCat: 'Ready-To-Cook',
+        prodSKU: 'SHRF-RTC-NBB',
+        prodImg: NasiBriyaniBukhari,
+        prodPrice: 17.60,
+        prodStock: 120,
+        prodStatus: 'active'
+    }
+]
+
+export default prodList;
