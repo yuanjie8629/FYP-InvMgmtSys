06b5a70c836d93246ffaa576cb7a6b745d54381c
Yuan Jie <yuanjie8629@gmail.com>
2022-02-28 03:12:43 +0800

    Changes in procfile and settings

--- invsys/settings.py
+++ invsys/settings.py
@@ -150,8 +150,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = "static/"
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "frontend/build/static")]
 
