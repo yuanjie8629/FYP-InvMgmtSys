8084b65a9904fd280e1b262361868143ad03541e
Yuan Jie <yuanjie8629@gmail.com>
2022-02-28 04:26:43 +0800

    Change project structure

--- invsys/settings.py
+++ invsys/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            os.path.join(BASE_DIR, "frontend/build"),
+            os.path.join(BASE_DIR, "build"),
             os.path.join(BASE_DIR, "templates"),
         ],
         "APP_DIRS": True,
@@ -154,7 +154,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "frontend/build/static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "build/static")]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
