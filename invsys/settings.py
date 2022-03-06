52d4ce0bf2791e086c1a51f70183cfe8b9dc5ee2
Yuan Jie <yuanjie8629@gmail.com>
2022-03-07 07:43:30 +0800

    test

--- invsys/settings.py
+++ invsys/settings.py
@@ -72,7 +72,6 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
