d3e9e72084378467bf633b0d62b23d32df804d2e
Yuan Jie <yuanjie8629@gmail.com>
2022-03-29 05:22:57 +0800

    Fixed Model wrongly copy

--- order/models.py
+++ order/models.py
@@ -1,6 +1,6 @@
-from enum import auto
 import random
 import string
+import uuid
 from django.db import models
 from core.models import SoftDeleteModel
 from customer.models import Cust
@@ -10,8 +10,23 @@ from shipment.models import OrderShipment
 from voucher.models import Voucher
 
 
+def create_id():
+    return "".join(random.choices(string.digits, k=15))
+
+
+def create_unique_id():
+    id = create_id()
+    unique = False
+    while not unique:
+        if not Order.objects.filter(pk=id).exists():
+            print(id)
+            return id
+        else:
+            id = create_id()
+
+
 class Order(SoftDeleteModel):
-    id = models.CharField(primary_key=True, max_length=40)
+    id = models.CharField(primary_key=True, max_length=40, default=create_unique_id)
     total_amt = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
     status = models.CharField(max_length=20, choices=ORDER_STATUS, blank=True)
     email = models.CharField(max_length=255, blank=True, null=True)
@@ -19,9 +34,7 @@ class Order(SoftDeleteModel):
     voucher = models.ForeignKey(
         Voucher, on_delete=models.DO_NOTHING, blank=True, null=True
     )
-    discount = models.DecimalField(
-        max_digits=10, decimal_places=2, blank=True, default=0
-    )
+    discount = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
     item = models.ManyToManyField(Item, through="OrderLine")
     shipment = models.ForeignKey(
         OrderShipment, on_delete=models.DO_NOTHING, related_name="order"
@@ -31,31 +44,19 @@ class Order(SoftDeleteModel):
         db_table = "order"
 
     def save(self, *args, **kwargs):
-        if self.id is None:
-            self.id = self.create_unique_id()
         if self.status is None:
             self.status = "unpaid"
         super(Order, self).save(*args, **kwargs)
         return self
-
-    def create_id(self):
-        return "".join(random.choices(string.digits, k=15))
-
-    def create_unique_id(self):
-        id = self.create_id()
-        unique = False
-        while not unique:
-            if not Order.objects.filter(pk=id).exists():
-                return id
-            else:
-                id = self.create_id()
 
 
 class OrderLine(models.Model):
     id = models.AutoField(primary_key=True)
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    special_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    special_price = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, null=True
+    )
     weight = models.DecimalField(max_digits=8, decimal_places=2)
     order = models.ForeignKey(
         Order, on_delete=models.CASCADE, related_name="order_line"
