842e702d97c467641514f039a7b109c395294ef0
Yuan Jie <yuanjie8629@gmail.com>
2022-03-29 05:22:57 +0800

    Fixed Model wrongly copy

--- order/migrations/0001_initial.py
+++ order/migrations/0001_initial.py
@@ -21,7 +21,7 @@ class Migration(migrations.Migration):
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('last_update', models.DateTimeField(auto_now=True)),
                 ('is_deleted', models.BooleanField(default=False)),
-                ('id', models.IntegerField(default=order.models.Order.create_unique_id, primary_key=True, serialize=False)),
+                ('id', models.IntegerField(primary_key=True, serialize=False)),
                 ('total_amt', models.DecimalField(blank=True, decimal_places=2, max_digits=10)),
                 ('status', models.CharField(blank=True, choices=[('completed', 'Completed'), ('cancel', 'Cancellation'), ('unpaid', 'Unpaid'), ('toShip', 'To Ship'), ('shipping', 'Shipping')], max_length=20)),
                 ('cust', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.DO_NOTHING, to='customer.cust')),
