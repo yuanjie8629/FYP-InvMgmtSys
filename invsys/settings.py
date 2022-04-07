b4eb06b252a344b5dbb38053f9bf3d5857472ed2
Yuan Jie <yuanjie8629@gmail.com>
2022-04-08 05:23:33 +0800

    test

--- invsys/settings.py
+++ invsys/settings.py
@@ -242,8 +242,7 @@ LOGGING = {
 AUTH_USER_MODEL = "core.Users"
 
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny"],
-    # "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
