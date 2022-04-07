93aacaa028f465fefb35e64caa45a6dadb8cc668
Yuan Jie <yuanjie8629@gmail.com>
2022-04-08 01:57:29 +0800

    fixed admin bug

--- src/api/services/adminAPI.ts
+++ src/api/services/adminAPI.ts
@@ -2,12 +2,12 @@ import axios from '@api/axiosInstance';
 import { getAdminInfo } from '@utils/storageUtils';
 
 export const adminDetailsAPI = () => {
-  return axios.get(`admin/${getAdminInfo()}/`);
+  return axios.get(`admin/${getAdminInfo()?.id}/`);
 };
 
 export const adminUpdateAPI = (data) => {
-  return axios.patch(`admin/${getAdminInfo()}/`, data);
+  return axios.patch(`admin/${getAdminInfo()?.id}/`, data);
 };
 
 export const changePassAPI = (data) =>
-  axios.patch(`admin/${getAdminInfo()}/change_pass/`, data);
+  axios.patch(`admin/${getAdminInfo()?.id}/change_pass/`, data);
