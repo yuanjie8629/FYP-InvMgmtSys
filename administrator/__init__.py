8e745d08a1cce18b825cca6ba10f0b8fa7b4c530
Yuan Jie <yuanjie8629@gmail.com>
2022-04-08 05:23:33 +0800

    test

--- administrator/__init__.py
+++ administrator/__init__.py
@@ -3,8 +3,9 @@ from django.urls import resolve
 
 
 def _request_creates_revision(request):
-    if resolve(request.path_info).url_name.endswith("login"):
-        return False
+    if resolve(request.path_info).url_name:
+        if resolve(request.path_info).url_name.endswith("login"):
+            return False
 
     return request.method not in ("OPTIONS", "GET", "HEAD")
 
