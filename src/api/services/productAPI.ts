4423d7a43947d2f667fc24a7dcefc6d1a4119f1f
Yuan Jie <yuanjie8629@gmail.com>
2022-04-10 00:46:16 +0800

    test fix product bug

--- src/api/services/productAPI.ts
+++ src/api/services/productAPI.ts
@@ -13,12 +13,19 @@ export const productPrevAllAPI = () => axios.get(`item/product/prev/all/`);
 export const productDetailsAPI = (id: string) =>
   axios.get(`item/product/${id}/`);
 
-export const productCreateAPI = async (data) =>
-  axios.post(`item/product/`, data, {
+export const productCreateAPI = (data) => {
+  let request = axios.post(`item/product/`, data, {
     headers: {
       'Content-type': 'multipart/form-data',
     },
   });
+  if (getAccessTknExpiry() < moment().unix()) {
+    refreshTknAPI().then(() => request);
+  } else {
+    return request;
+  }
+  return;
+};
 
 export const productUpdAPI = (id, data) =>
   axios.patch(`item/product/${id}/`, data, {
