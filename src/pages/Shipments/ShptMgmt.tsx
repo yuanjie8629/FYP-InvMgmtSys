97a54f1c640e5720b6043453aa7860b57f1341e0
Yuan Jie <yuanjie8629@gmail.com>
2022-04-14 04:56:10 +0800

    update gif for shptmgmt

--- src/pages/Shipments/ShptMgmt.tsx
+++ src/pages/Shipments/ShptMgmt.tsx
@@ -2,7 +2,7 @@ import ComingSoon from '@components/Layout/ComingSoon';
 
 const ShptMgmt = () => {
   return (
-    <ComingSoon img='https://res.cloudinary.com/yuanjie/image/upload/v1649580966/CommingSoon/Help_cdfh8i.gif' />
+    <ComingSoon img='https://res.cloudinary.com/yuanjie/image/upload/v1649580971/CommingSoon/Truck_bxitsj.gif' />
   );
 };
 
