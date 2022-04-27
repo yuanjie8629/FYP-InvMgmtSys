fb928cf56cabff3935607787028f1cd55d0cc8b4
Yuan Jie <yuanjie8629@gmail.com>
2022-04-27 16:31:17 +0800

    signal status update

--- voucher/apps.py
+++ voucher/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class VoucherConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'voucher'
+
+    def ready(self):
+            from voucher import signals
